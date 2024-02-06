735d6b332d77ca80aafa09dab3aaf8458711cc3f
Abhishek Kumar <157613182+aavviii@users.noreply.github.com>
2024-02-06 23:10:36 +0530

    Update auto massage type.py

--- auto massage type.py
+++ auto massage type.py
@@ -5,7 +5,7 @@ import pyautogui as pg
 msg = input("type the massage you want to send?\n")
 n = int(input('How many times you want to send the message?\n'))
 print('You have 30 seconds to open the chat where you want to send the message.')
-time.sleep(30)
+time.sleep(10)
 
 for i in range(n):
     pg.write(msg)
